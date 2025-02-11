b0195280e2ea5828be08f7fc35ae7e4283b16ab1
Agris <7924@inbox.lv>
2025-02-11 19:11:09 +0200

    add googletags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    @if(app()->isProduction())
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CE7KZTTZ2J"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CE7KZTTZ2J');
+        </script>
+    @endif
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enterprise Intelligence Solutions</title>
